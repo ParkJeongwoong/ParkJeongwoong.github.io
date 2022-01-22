8e25c60b76089fb5a79953521e840b37c022e6b7
dvlprjw <dvlprjw@gmail.com>
2022-01-22 13:21:54 +0900

    .env test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import { StateProvider } from "store/store";
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  console.log("환경변수1 : " + process.env.REACT_APP_SERVER_URL);
+  console.log("환경변수2 : " + secrets.SERVER_URL);
   return (
     <StateProvider>
       <div className="App">
