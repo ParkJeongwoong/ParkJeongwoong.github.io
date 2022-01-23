4cc0e7e6243f84bb09ff7177f8359a1d77b7df1f
dvlprjw <dvlprjw@gmail.com>
2022-01-23 18:17:36 +0900

    환경변수 조정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,9 @@ import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   console.log("환경변수1 : " + process.env.REACT_APP_SERVER_URL);
-  console.log("환경변수2 : " + secrets.SERVER_URL);
+  console.log("환경변수2 : " + process.env.REACT_APP_SERVER_URL2);
+  console.log("환경변수3 : " + process.env.REACT_APP_SERVER_URL3);
+  // console.log("환경변수2 : " + secrets.SERVER_URL);
   return (
     <StateProvider>
       <div className="App">
